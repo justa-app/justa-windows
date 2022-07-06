fe2ca00b9a417da0a365b9810de301f22be8f1d0
litalshl <lital.shalev@gmail.com>
2022-07-06 09:05:36 +0300

    Create initial user control for start session

---
+++ WindowsApplication/StartSessionUserControl.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+
+
+namespace WindowsApplication
+{
+    /// <summary>
+    /// Interaction logic for StartSessionUserControl.xaml
+    /// </summary>
+    public partial class StartSessionUserControl : UserControl
+    {
+        public StartSessionUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
